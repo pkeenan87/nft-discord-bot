76e21cf6e8e7e809b8c014b80169e9960e085c86
Patrick Keenan <patrick.keenan8787@gmail.com>
2023-04-10 11:27:48 -0400

    added sale and listing notification

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const { Client, IntentsBitField, EmbedBuilder } = require('discord.js')
 const axios = require('axios');
+const { OpenSeaStreamClient } = require('@opensea/stream-js');
+const { WebSocket } = require('ws');
 require('dotenv').config()
 
 function roundToTwo(num) {
@@ -14,6 +16,144 @@ const config = {
   }
 };
 
+const openseaClient = new OpenSeaStreamClient({
+  token: process.env.OPENSEA_KEY,
+  connectOptions: {
+    transport: WebSocket
+  }
+});
+
+openseaClient.connect();
+
+// Item Listed
+openseaClient.onItemListed(process.env.OPENSEA_COLLECTION_SLUG, async (event) => {
+  console.log(event)
+  const address = event.payload.item.nft_id.split("/")[1]
+  const tokenId = event.payload.item.nft_id.split("/")[2]
+
+
+  // console.log(event)
+  // https://discord.com/api/webhooks/1091780339636125839/9YNAfYSHOyZ0J0vVhFtsQuwGdo_aZUwvwHbjewvnKC0tXoDLqGD18xe6eWcd05NFRSsn
+  const balanceInEth = (event.payload.base_price / 1e18) / event.payload.quantity
+  const usdBalance = roundToTwo(balanceInEth * event.payload.payment_token.usd_price)
+
+  console.log(`ETH Balance: ${balanceInEth}`)
+  console.log(`USD Balance: ${usdBalance}`)
+  console.log(`Seller: ${event.payload.maker.address}`)
+  console.log(`OpenseaLink: ${event.payload.item.permalink}`)
+
+  // Sleep for Two Seconds to avoid throttling
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
+
+  const response = await axios.get(`https://api.opensea.io/api/v1/asset/${address}/${tokenId}?format=json`, config)
+
+
+  let payload = {
+    "content": null,
+    "embeds": [
+      {
+        "title": "New Item Listed :partying_face:",
+        "color": 5814783,
+        "fields": [
+          {
+            "name": "ETH Price",
+            "value": `${balanceInEth}`
+          },
+          {
+            "name": "USD Price",
+            "value": `$ ${usdBalance}`
+          },
+          {
+            "name": "Quantity",
+            "value": `${event.payload.quantity}`
+          },
+          {
+            "name": "Seller",
+            "value": `${event.payload.maker.address}`
+          },
+          {
+            "name": "OpenSea Link",
+            "value": `${event.payload.item.permalink}`
+          }
+        ],
+        "image": {
+          "url": `${response.data.image_url}`
+        }
+      }
+    ],
+    "attachments": []
+  }
+
+  console.log(payload)
+  await axios.post(process.env.DISCORD_WEBHOOK_LINK, payload)
+});
+
+// Item Sold
+openseaClient.onItemSold(process.env.OPENSEA_COLLECTION_SLUG, async (event) => {
+  console.log(event)
+  // Token Info
+  const address = event.payload.item.nft_id.split("/")[1]
+  const tokenId = event.payload.item.nft_id.split("/")[2]
+
+  // Payment Info
+  const balanceInEth = (event.payload.sale_price / 1e18) / event.payload.quantity
+  const usdBalance = roundToTwo(balanceInEth * event.payload.payment_token.usd_price)
+
+
+  // Sleep for Two Seconds to avoid throttling
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
+
+  const response = await axios.get(`https://api.opensea.io/api/v1/asset/${address}/${tokenId}?format=json`, config)
+
+  // Build Embed
+  let payload = {
+    "content": null,
+    "embeds": [
+      {
+        "title": "New Item Sold :partying_face:",
+        "color": 5814783,
+        "fields": [
+          {
+            "name": "ETH Sale Price",
+            "value": `${balanceInEth}`
+          },
+          {
+            "name": "USD Sale Price",
+            "value": `$ ${usdBalance}`
+          },
+          {
+            "name": "Quantity",
+            "value": `${event.payload.quantity}`
+          },
+          {
+            "name": "Seller",
+            "value": `${event.payload.maker.address}`
+          },
+          {
+            "name": "Buyer",
+            "value": `${event.payload.taker.address}`
+          },
+          {
+            "name": "Etherscan Link",
+            "value": `https://etherscan.io/tx/${event.payload.transaction.hash}`
+          },
+          {
+            "name": "OpenSea Link",
+            "value": `${event.payload.item.permalink}`
+          }
+        ],
+        "image": {
+          "url": `${response.data.image_url}`
+        }
+      }
+    ],
+    "attachments": []
+  }
+
+  await axios.post(process.env.DISCORD_WEBHOOK_LINK, payload)
+});
 
 const client = new Client({
   intents: [
